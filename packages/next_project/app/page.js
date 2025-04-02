5c7e811d0d4dc5e68c314a26ac0f5ede89654c5d
prakash02220 <parkash.mishra@webspero.com>
2025-04-02 09:46:42 +0530

    initial commit

---
+++ packages/next_project/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import { MyButton } from "@your-org/ui_comp";
+export default function Home() {
+  return (
+   <>
+   "parkash"
+   <MyButton variant="outlined" backgroundColor="black">Mishra</MyButton>
+   </>
+  );
+}
